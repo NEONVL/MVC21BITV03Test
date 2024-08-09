d4ef7fc5537ddce587b0e7cd5b154f849c900e3c
hieudut <136869346+hieudut@users.noreply.github.com>
2024-08-09 15:16:45 +0700

    Câu 2: Thêm mới 1 thành phần - Part

---
+++ PartController.cs
@@ -0,0 +1,42 @@
+﻿using HieuLinhMidTest.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace HieuLinhMidTest.Controllers
+{
+    public class PartController : Controller
+    {
+        private readonly CarDealerContext _context;
+
+
+        public PartController(CarDealerContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Parts.Include(s => s.Supplier).ToList();
+
+            return View(data);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Suppliers = new SelectList(_context.Suppliers.ToList(), "Id", "Name");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Part part, IFormFile ProductImage)
+
+
+        {
+            ViewBag.Suppliers = new SelectList(_context.Suppliers.ToList(), "Id","Name");
+
+            _context.Add(part);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}

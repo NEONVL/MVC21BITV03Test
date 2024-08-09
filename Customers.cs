525edef985b30a7ee48489979bcc45976a5e9093
NEONVL <147905296+NEONVL@users.noreply.github.com>
2024-08-09 14:43:02 +0700

    thêm mới 1 khách hàng

---
+++ Customers.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HieuLinhMidTest.Models
+{
+    [Table("Customers")]
+    public class Customers
+    {
+        [Key]
+        public int ID { get; set; }
+        
+        public string? Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool IsYoungDriver { get; set; }
+    }
+}
